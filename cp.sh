75ad091d4f7238341c77ea4200a6d0870335a417
Daniel YC Lin <dlin.tw@gmail.com>
2019-06-01 17:01:46 +0800

    backup log on mobile

--- cp.sh
+++ cp.sh
@@ -6,4 +6,7 @@ cd "$from_dir"
 adb shell "mkdir -p $to_dir"
 adb push index.html index.js settings.js "$to_dir"
 echo Done
+adb pull /sdcard/Robotmon/robotmon.log robotmon.$(date +'%Y%m%d%H%M%S').log
+
+adb shell rm /sdcard/Robotmon/robotmon.log
 # vim:et sw=2 ts=2 ai nocp sta
